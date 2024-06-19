0f6877e371a17f2d09d30eafa371e25d4deb1367
Joba Adewumi <jobaadewumis@gmail.com>
2024-06-19 22:22:11 +0100

    Tweaked supplier model

--- api/mysite/supplier/models.py
+++ api/mysite/supplier/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 class Supplier(models.Model):
     name = models.CharField(max_length=100)
-    contact_info = models.BigIntegerField()
+    contact_info = models.CharField(max_length=11)
